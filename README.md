167d97370b158881abee12c9bb908ae6451183e3
Cason Adams <casonadams@gmail.com>
2023-05-24 01:52:14 -0600

    add: quick lookup by name, and other improvements

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # bitwarden-cli
 
-A zsh Bitwarden CLI fuzzy finder using `skim`
+A zsh Bitwarden CLI fuzzy finder using `fzf`
 
 ## requirements
 
 - [bw](https://bitwarden.com/download/)
+- [fzf](https://github.com/junegunn/fzf)
 - [jq](https://stedolan.github.io/jq/)
-- [sk](https://github.com/lotabout/skim#package-managers)
-
-*Note* you can use `fzf` just create an `alias sk=fzf`
 
 ## setup
 
@@ -34,28 +32,16 @@ plugins+=(
 
 ### login
 
-This should only need to be run once. And there might
-be better ways to `login` and `logout` that are more
-secure. This is just an example of what is needed
-to get this plugin to work
-
 ```sh
-echo "export BW_SESSION=$(bw unlock --raw)" >> ~/.zprofile
-exec zsh
+bw login
 ```
 
-### linux alias
-
-```~/.zshrc
-alias pbcopy='xsel -i --clipboard'
-alias pbpaste='xsel -o --clipboard'
-```
+** Note if having issues remove /tmp/.bw* file **
 
 ### environment (optional)
 
 - BW_PASSWORD_KEY (default `^[p`)
 - BW_TOTP_KEY     (default `^[t`)
-- BW_AUTO_COPY    (default `true`)
 
 ## usage (defaults)
 
--- bitwarden-cli.plugin.zsh
+++ bitwarden-cli.plugin.zsh
@@ -1,37 +1,115 @@
 local BW_PASSWORD_KEY="${BW_PASSWORD_KEY:-^[p}"
 local BW_TOTP_KEY="${BW_TOTP_KEY:-^[t}"
-local BW_AUTO_COPY="${BW_AUTO_COPY:-true}"
 
-function .copy() {
-  local result=$1
+function __bw_copy() {
+    local result=$1
 
-  if $BW_AUTO_COPY; then
-    printf "%s" $result | pbcopy
+    if [[ $(uname) == "Darwin" ]]; then
+        printf "%s" $result | pbcopy
+    else
+        printf "%s" $result | xsel -i --clipboard
+    fi
+}
+
+function __bw_get_id() {
+    local mode=$1
+    local search=""
+    local token=$(__bw_session_token)
+
+    if [[ ! -v $2 ]]; then
+        search="-q $2"
+    fi
+
+    jq -r -c '.[] | .name + "/" + .login.username + " │ " + .id' <(bw list items --session $token) \
+        | fzf ${search} -1 --preview-window=:nohidden --preview-window=right:0% --prompt="bw ${mode}> " \
+        | cut -f2 -d "│" \
+        | tr -d " "
+}
+
+function __bw_session_token() {
+    local file_path="/tmp/.bw-$(whoami)-$(hostname)-42"
+    local token
+
+    if [[ ! -f "$file_path" ]]; then
+        token=$(__bw_generate_unlock_token)
+    fi
+
+    if [[ $(uname) == "Darwin" ]]; then
+        modification_time=$(stat -f "%m" "$file_path")
+    else
+        modification_time=$(stat -c "%Y" "$file_path")
+    fi
+
+    local current_time=$(date +%s)
+    local age=$((current_time - modification_time))
+
+    if [[ $age -gt 900 ]]; then
+        token=$(__bw_generate_unlock_token)
+    else
+        token=$(cat $file_path)
+    fi
+
+    echo $token
+}
+
+function __bw_generate_unlock_token() {
+    token=$(bw unlock --raw)
+    bw sync --session $token
+    echo -n $token > $file_path
+    echo -n $token
+}
+
+function bw_pass() {
+    local subcommand=${1}
+
+    if [[ "${subcommand}" = "otp" ]]; then
+        shift
+        __bw_totp "${@}"
+    elif [[ "${subcommand}" = "list" ]]; then
+        __bw_list "${@}"
+    else
+        __bw_pass "${@}"
+    fi
+}
+
+function __bw_totp() {
+    local token=$(__bw_session_token)
+
+    if [[ "$1" -eq "-c" ]]; then
+        __bw_copy $(BW_SESSION=$token bw get totp "$(__bw_get_id totp)")
+    else
+        BW_SESSION=$token bw get totp "$(__bw_get_id totp)"
+    fi
+}
+
+function __bw_pass() {
+    local token=$(__bw_session_token)
+
+    if [[ "$1" = "-c" ]]; then
+        __bw_copy $(BW_SESSION=$token bw get password "$(__bw_get_id password $2)")
+    else
+        BW_SESSION=$token bw get password "$(__bw_get_id password $1)"
+    fi
+}
+
+function __bw_list() {
+    local token=$(__bw_session_token)
+
+    jq -r -c '.[] | .name + "/" + .login.username' <(bw list items --session $token)
+}
+
+function __bw_password(){
+    bw_pass -c
     zle accept-line
-  else
-    read -s "reply?$result" < /dev/tty
-    zle reset-prompt
-  fi
 }
 
-function .bw_get_id() {
-  local mode=$1
-  jq -r -c '.[] | "[" + .name + "](" + .login.username + ")  │  " + .id' <(bw list items) \
-    | sk --preview-window=:nohidden --preview-window=right:0% --prompt="bw ${mode}> " \
-    | cut -f2 -d "│" \
-    | tr -d " "
+function __bw_otp(){
+    bw_pass otp -c
+    zle accept-line
 }
 
-function bw_get_password() {
-  .copy $(bw get password "$(.bw_get_id password)")
-}
+zle -N "__bw_password"
+zle -N "__bw_otp"
 
-function bw_get_totp() {
-  .copy $(bw get totp "$(.bw_get_id totp)")
-}
-
-zle -N bw_get_password
-zle -N bw_get_totp
-
-bindkey "${BW_PASSWORD_KEY}" bw_get_password
-bindkey "${BW_TOTP_KEY}" bw_get_totp
+bindkey "${BW_PASSWORD_KEY}" __bw_password
+bindkey "${BW_TOTP_KEY}" __bw_otp
